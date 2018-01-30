78cb0337b6fe23665ab7489a3ba67e4ceb7d45f5
arthur <arthur.teisseire@epitech.eu>
2018-01-30 14:31:07 +0100

    Now arguments are checked as expected

--- src/error_handling/check_args.c
+++ src/error_handling/check_args.c
@@ -9,7 +9,16 @@
 
 int check_args(char **args)
 {
-	if (my_str_isnum(args[0]) && my_str_isnum(args[1]))
-		return (0);
-	return (-1);
+	int arg0;
+	int arg1;
+
+	arg0 = my_atoi(args[0]);
+	arg1 = my_atoi(args[1]);
+	if (!my_str_isnum(args[0]) && !my_str_isnum(args[1]))
+		return (-1);
+	if (arg0 <= 1 || arg0 >= 100)
+		return (-1);
+	if (arg1 <= 0)
+		return (-1);
+	return (0);
 }
